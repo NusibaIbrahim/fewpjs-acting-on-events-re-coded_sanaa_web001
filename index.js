ebe452b8c8dd15b7f9080b4f63e2b7709cad1b1b
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-04 11:06:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ let dodger = document.getElementById('dodger');
 function moveDodgerLeft(){
   var leftN = dodger.style.left.replace("px", "");
   left = parseInt(leftN,10);
-  if(left >10)  
+  if(left >0){
+    
+  }  
 }
